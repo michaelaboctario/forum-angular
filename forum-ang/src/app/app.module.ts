c78537af4fc880acc7a13bab81e6fd46c131818f
Michaela Boctario <mbucatariu24@gmail.com>
2022-06-05 20:58:26 +0200

    add public users list

--- forum-ang/src/app/app.module.ts
+++ forum-ang/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { UsersComponent } from './users/users.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- forum-ang/src/app/user.service.ts
+++ forum-ang/src/app/user.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { User } from './user';
 import { USERS } from './mock-users';
 import { Observable, of } from 'rxjs';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private usersUrl = 'http://localhost:3000/api/users';  // URL to web api
+
+  constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return of(USERS);
+    return this.http.get<User[]>(this.usersUrl);
   }
+
 }
